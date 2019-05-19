a990c9fc016b27fd4c8aa68b04841e4098361296
DESKTOP-PPILMMS\Windows <odair.viol@gmail.com>
2019-05-18 23:37:19 -0300

    Commit inicial do projeto base

---
+++ app/src/androidTest/java/omdbapp/odair/sdm/ifsp/edu/br/omdbapp/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package omdbapp.odair.sdm.ifsp.edu.br.omdbapp
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("omdbapp.odair.sdm.ifsp.edu.br.omdbapp", appContext.packageName)
+    }
+}
